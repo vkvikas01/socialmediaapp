f7e397c91426c910f8c50b75ffe00e1887debdb1
VARUNSHARMA55665 <varunsharma55665@gmail.com>
2021-04-01 00:59:58 +0530

    Added one more route(users.js) and controller(user_controller.js)

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,9 @@ const homeController = require('../controllers/home_controller');
 console.log('router loaded');
 
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
+
+// for further routes excess from here
+// router.use('/routerName',require('./routerfile));
 
 module.exports = router;
