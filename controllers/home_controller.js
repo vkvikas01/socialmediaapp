525b7617c76ef84f0e1d71028661db9d08a8a363
VARUNSHARMA55665 <varunsharma55665@gmail.com>
2021-04-01 01:15:51 +0530

    Installed ejs and setup view engine

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,7 @@
 module.exports.home = function(req, res){
-    res.end('<h1>Express is up for Codeial</h1>');
+    // res.end('<h1>Express is up for Codeial</h1>');
+    // insted of home we can also write ../views/home it will also run
+    return res.render('home',{
+        title: "Home"
+    });
 }
