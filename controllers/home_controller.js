c9d70d5ee09303302fb13a5f83399bee7dbd4042
VARUNSHARMA55665 <varunsharma55665@gmail.com>
2021-05-03 04:36:04 +0530

    Database Relation post and related comments & created schema , saved post and Displayed post and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,11 +1,26 @@
+const Post = require('../models/post.js');
+
 module.exports.home = function(req, res){
     // res.end('<h1>Express is up for Codeial</h1>');
     // insted of home we can also write ../views/home it will also run
-    console.log(req.cookies);
-    res.cookie('user_id', 25);
-    return res.render('home',{
-        title: "Home"
-    });
+
+    // console.log(req.cookies);
+    // res.cookie('user_id', 25);
+    // Post.find({}, function(err,posts){
+    //     return res.render('home',{
+    //         title: "Codeial | Home",
+    //         posts: posts
+    //     });
+    // })
+
+    // Populate the user of each post
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+            title: "Codeial | Home",
+            posts: posts
+        });
+    })
+    
 }
 
 // module.exports.actionName = function(req,res){}
