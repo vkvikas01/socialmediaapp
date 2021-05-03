e70bae5ee3fc50fe3ef847e958a5e87a75bf0cad
VARUNSHARMA55665 <varunsharma55665@gmail.com>
2021-05-03 08:46:54 +0530

    Check Authentication on creating post and Check schema for comments

--- models/post.js
+++ models/post.js
@@ -10,6 +10,11 @@ const postSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
+    },
+    // include array of ids of all comments in this post schema itself.
+    comment: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'comment'
     }
 },{
     timestamps: true
