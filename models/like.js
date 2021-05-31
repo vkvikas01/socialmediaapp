a1fc28916865b00b647732b385d0b08d97cfbc8d
VARUNSHARMA55665 <varunsharma55665@gmail.com>
2021-06-01 01:43:05 +0530

    Schema Setup :: Likes

---
+++ models/like.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const likeSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.ObjectId,
+    },
+    likeable: {
+        type: mongoose.Schema.ObjectId,
+        require: true,
+        refPath: 'onModel'
+    },
+    onModel: {
+        type: String,
+        required: true,
+        emum: ['Post','Comment']
+    }
+},{
+    timestamps: true
+});
+
+const Like = mongoose.model('Like', likeSchema);
+module.exports = Like;
