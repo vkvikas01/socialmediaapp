1899c7c6459165658dc519db19d8569121c3ceb4
VARUNSHARMA55665 <varunsharma55665@gmail.com>
2021-06-09 05:18:22 +0530

    Environment Varibles :: Production Environment

--- config/environment.js
+++ config/environment.js
@@ -22,7 +22,30 @@ const development = {
 }
 
 const production = {
-    name: 'production'
+    name: 'production',
+    asset_path: process.env.CODEIAL_ASSET_PATH,
+    // asset_path: '/assets'
+    session_cookie_key: process.env.CODEIAL_SESSION_COOKIE_KEY,
+    db: process.env.CODEIAL_DB,
+    smtp: {
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: 'varunsharma55665@gmail.com',
+            pass: ''
+        }
+    },
+    google_client_id: process.env.CODEIAL_GOOGLE_CLIENT_ID,
+    google_client_secret: process.env.CODEIAL_GOOGLE_CLIENT_SECRET,
+    google_call_back_url: process.env.CODEIAL_GOOGLE_CALLBACK_URL,
+    jwt_secret: process.env.CODEIAL_JWT_SECRET
 }
 
-module.exports = development;
+console.log(process.env.CODEIAL_ENVIRONMENT);
+
+// module.exports = development;
+
+// npm install -g win-node-env use this to run the ===== npm run prod_start      (-g in the install command to install package globally)
+module.exports = eval(process.env.CODEIAL_ENVIRONMENT) == undefined?development:eval(process.env.CODEIAL_ENVIRONMENT);
