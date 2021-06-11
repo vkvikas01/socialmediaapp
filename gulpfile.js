2986ef3cdc8af8d84eb0148d68fcb874f9d8e1d3
VARUNSHARMA55665 <varunsharma55665@gmail.com>
2021-06-12 01:52:20 +0530

    Completing Gulp :: Js and Image Minification

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,12 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const cssnano = require('gulp-cssnano');
 const rev = require('gulp-rev');
+const uglify = require('gulp-uglify-es').default;
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+const { publicDecrypt } = require('crypto');
 
-gulp.task('css', function(){
+gulp.task('css', function(done){
     console.log('minifying css....');
     gulp.src('./assets/sass/**/*.scss')
     .pipe(sass())
@@ -13,7 +17,7 @@ gulp.task('css', function(){
 
 // change CODEIAL_ASSETS_PATH from ./assets to ./public/assets
 
-    return gulp.src('./assets/**/*.css')
+    gulp.src('./assets/**/*.css')
     .pipe(rev())
     .pipe(gulp.dest('./public/assets'))  // It will put in css folder
     .pipe(rev.manifest({
@@ -21,4 +25,46 @@ gulp.task('css', function(){
         merge: true
     }))
     .pipe(gulp.dest('./public/assets'));
-})
+
+    done();
+
+});
+
+gulp.task('js', function(done){
+    console.log('minifing js....');
+    gulp.src('./assets/**/*.js')
+    .pipe(uglify())
+    .pipe(rev())
+    .pipe(gulp.dest('./public/assets'))
+    .pipe(rev.manifest({
+        cwd: 'public',
+        merge: true
+    }))
+    .pipe(gulp.dest('./public/assets'));
+    done();
+});
+
+gulp.task('images', function(done){
+    console.log('Compressing images....');
+    gulp.src('./assets/**/*.+(png|jpg|svg|jpeg)')
+    .pipe(imagemin())
+    .pipe(rev())
+    .pipe(gulp.dest('./public/assets'))
+    .pipe(rev.manifest({
+        cwd: 'public',
+        merge: true
+    }))
+    .pipe(gulp.dest('./public/assets'));
+    done();
+});
+
+// empty the public/assets directory (whenever we are building the prohect we need to clear the previous build % build it from scratch)
+gulp.task('clean:assets',function(done){
+    del.sync('./public/assets');
+    done();
+});
+
+gulp.task('build', gulp.series('clean:assets', 'css', 'js', 'images') , function(done){
+    console.log('Building assets');
+    done();
+});
